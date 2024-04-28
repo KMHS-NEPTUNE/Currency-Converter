1bbf7101bf0ae8f58aeccab17f6c8d7e4d03774b
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-04-28 17:00:56 +0900

    Merge remote-tracking branch 'origin/main'

--- main.py
+++ main.py
@@ -36,19 +36,19 @@ def excel_exchange(file: Path):
         elif i == excel_list[1]:
             continue
         name = i[0]
-        KRW = round(i[1])
-        JPY = round(i[2])
-        JPY_to_KRW = round(JPY * exchange_rate)
+        krw = round(i[1])
+        jpy = round(i[2])
+        jpy_to_krw = round(jpy * exchange_rate)
 
         sheet = wb.get_sheet_by_name(wb.sheetnames.pop(1))
         sheet.cell(row=count, column=1).value = name
         sheet.cell(row=count, column=1).style = "main"
 
-        sheet.cell(row=count, column=2).value = KRW
+        sheet.cell(row=count, column=2).value = krw
         sheet.cell(row=count, column=2).style = "main"
         sheet.cell(row=count, column=2).number_format = f_KRW_sing
 
-        sheet.cell(row=count, column=3).value = JPY_to_KRW
+        sheet.cell(row=count, column=3).value = jpy_to_krw
         sheet.cell(row=count, column=3).style = "main"
         sheet.cell(row=count, column=3).number_format = f_KRW_sing
 
@@ -56,21 +56,21 @@ def excel_exchange(file: Path):
         sheet.cell(row=count, column=1).value = name
         sheet.cell(row=count, column=1).style = "main"
 
-        if i[1] - JPY_to_KRW < 0:
-            sheet.cell(row=count, column=2).value = (KRW - JPY_to_KRW) * -1
+        if i[1] - jpy_to_krw < 0:
+            sheet.cell(row=count, column=2).value = (krw - jpy_to_krw) * -1
             sheet.cell(row=count, column=2).style = "main"
             sheet.cell(row=count, column=2).number_format = f_KRW_sing
         else:
-            sheet.cell(row=count, column=2).value = KRW - JPY_to_KRW
+            sheet.cell(row=count, column=2).value = krw - jpy_to_krw
             sheet.cell(row=count, column=2).style = "main"
             sheet.cell(row=count, column=2).number_format = f_KRW_sing
 
-        if (i[1] - JPY_to_KRW) / i[1] * 100 < 0:
-            sheet.cell(row=count, column=3).value = round((i[1] - JPY_to_KRW) / i[1] * 100 * -1, 2)
+        if (i[1] - jpy_to_krw) / i[1] * 100 < 0:
+            sheet.cell(row=count, column=3).value = round((i[1] - jpy_to_krw) / i[1] * 100 * -1, 2)
             sheet.cell(row=count, column=3).style = "percent"
             sheet.cell(row=count, column=3).number_format = f_percent
         else:
-            sheet.cell(row=count, column=3).value = round((i[1] - JPY_to_KRW) / i[1] * 100, 2)
+            sheet.cell(row=count, column=3).value = round((i[1] - jpy_to_krw) / i[1] * 100, 2)
             sheet.cell(row=count, column=3).style = "percent"
             sheet.cell(row=count, column=3).number_format = f_percent
 
@@ -80,7 +80,8 @@ def excel_exchange(file: Path):
 
 
 def excel_snack(file: Path):
-    pass
+    print(file)
+
 
 @app.command(help="환율 계산기")
 def main(file: Annotated[Path, typer.Argument(help="Excel 파일 경로")],
